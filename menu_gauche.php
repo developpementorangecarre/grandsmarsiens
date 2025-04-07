586010c9284db0457e05ef0d6c5a687550945b64
Loan THOMY <loan.thomy@gmail.com>
2025-04-07 11:47:30 +0200

    fix utf8 menu_gauche

--- menu_gauche.php
+++ menu_gauche.php
@@ -19,7 +19,7 @@
 
   <li><a href="ventes.php">Ventes&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
 
-  <li><a href="articles.php">Dépot&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
+  <li><a href="articles.php">DÃ©pot&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
 
   <li><a href="retraits.php">Retrait&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
 
@@ -41,13 +41,13 @@
 </form>
 			
 <h2 class="title1">
-<table width=100%><tr><td width=80% align=right>Personne n° </td><td> <form action=liste_personne.php method=get><input type=text size=3 name=id></form></td></tr></table>
+<table width=100%><tr><td width=80% align=right>Personne nÂ° </td><td> <form action=liste_personne.php method=get><input type=text size=3 name=id></form></td></tr></table>
 </h2>
 <h2 class="title1">
-<table width=100%><tr><td width=80% align=right>Dépot n° </td><td> <form action=depot_articles.php method=get><input type=hidden name=bourse_active value=OUI><input type=text size=3 name=reference_article></form></td></tr></table>
+<table width=100%><tr><td width=80% align=right>DÃ©pot nÂ° </td><td> <form action=depot_articles.php method=get><input type=hidden name=bourse_active value=OUI><input type=text size=3 name=reference_article></form></td></tr></table>
 </h2>
 <h2 class="title1">
-<table width=100%><tr><td width=80% align=right>Retrait du dépôt n° </td><td> <form action=retrait_cocher_articles.php method=get><input type=hidden name=bourse_active value=OUI><input type=text size=3 name=reference_article></form></td></tr></table>
+<table width=100%><tr><td width=80% align=right>Retrait du dÃ©pÃ´t nÂ° </td><td> <form action=retrait_cocher_articles.php method=get><input type=hidden name=bourse_active value=OUI><input type=text size=3 name=reference_article></form></td></tr></table>
 </h2>
 </div>
 
